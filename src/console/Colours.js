1c10da38d19ee9e375a195edc498c1da878ee123
Abe Train <abraham.train@gmail.com>
2021-12-20 00:24:38 -0500

    Added remove colour button

--- src/console/Colours.js
+++ src/console/Colours.js
@@ -1,24 +1,19 @@
 import { HexColorPicker } from "react-colorful";
 import { useEffect, useState } from "react";
 
+// TODO Make the picker useable with only keyboard?
 
-const Picker = ({ setShow, colour, setColour }) => {
-  // TODO Make the picker useable with only keyboard
-
-  function selectColour() {
-    setShow(false);
-  }
+const Picker = ({ colour, setColour }) => {
 
   return (
-    <div id="pallet" >
+    <div >
       <HexColorPicker color={colour} onChange={setColour} />
-      <button onClick={selectColour}>Select</button>
     </div>
   )
 };
 
 function ColourSquare({ squareColour, setColour, squareNumber,
-  isActive, activate, activeColour }) {
+  isActive, setSquare, activeColour }) {
 
   const [show, setShow] = useState(false);
 
@@ -32,7 +27,7 @@ function ColourSquare({ squareColour, setColour, squareNumber,
       setShow(false);
     } else {
       setColour(squareColour);
-      activate(squareNumber);
+      setSquare(squareNumber);
       setShow(true);
     }
   }
@@ -57,8 +52,11 @@ function ColourSquare({ squareColour, setColour, squareNumber,
       <li className="colour-square" style={style} onClick={activateSquare}></li>
       {
         show &&
-        <Picker show={show} setShow={setShow} colour={activeColour}
-          setColour={setColour} />
+        <div  id="pallet">
+          <Picker show={show} setShow={setShow} colour={activeColour}
+            setColour={setColour} />
+            <button onClick={() => setShow(false)}>Select</button>
+        </div>
       }
     </div>
   )
@@ -67,13 +65,21 @@ function ColourSquare({ squareColour, setColour, squareNumber,
 
 export function Colours({ coloursList, setColoursList }) {
 
-  const [colour, setColour] = useState("#aabbcc");
+  const [colour, setColour] = useState("#7F95D1");
   const [square, setSquare] = useState(null);
-
 
   function addColour() {
     setColoursList([...coloursList, colour]);
     setSquare(coloursList.length);
+  }
+
+  function removeColour() {
+    if (square) {
+      const newList = [...coloursList];
+      newList.splice(square, 1);
+      setColoursList(newList);
+      setSquare(null);
+    }
   }
 
   // Change colour using useEffect
@@ -82,9 +88,8 @@ export function Colours({ coloursList, setColoursList }) {
   useEffect(() => {
     const newList = [...coloursList];
     newList.splice(square, 1, colour);
-    console.log(newList);
     setColoursList(newList);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [colour])
 
 
@@ -92,12 +97,17 @@ export function Colours({ coloursList, setColoursList }) {
     <div className="colours">
       <h2>Colours</h2>
       <button onClick={addColour}>Add</button>
+      <button onClick={removeColour}>Real Remove</button>
       <ul className="colours-list">
         {coloursList.map((squareColour, index) => {
           const isActive = index === square;
-          return <ColourSquare key={index} setColour={setColour} squareNumber={index} activate={setSquare} activeColour={colour} squareColour={squareColour} isActive={isActive} />
+          return <ColourSquare key={index} setColour={setColour} squareNumber={index}
+          setSquare={setSquare} activeColour={colour} squareColour={squareColour} isActive={isActive} />
         })}
       </ul>
+      {(coloursList.length < 3) && 
+      <p>Please have at least 2 colours.</p>
+      }
     </div>
   )
 }
